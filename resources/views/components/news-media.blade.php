0c0d40ad0d9d611022bcd508b8efe223a7dfc2c6
Yui <rolantwin@gmail.com>
2026-09-25 17:51:23 +0700

    Keep transparent fallback mark

--- resources/views/components/news-media.blade.php
+++ resources/views/components/news-media.blade.php
@@ -1,5 +1,5 @@
 @props(['article' => null, 'size' => 'card'])
 @php($featured = $article?->featuredMedia?->media)
 <div class="news-media news-media--{{ $size }}" role="img" aria-label="{{ $featured?->alt_text ?: 'Visual artikel Harian Merah Putih' }}">
-    @if ($featured)<img src="{{ $featured->url() }}" alt="{{ $featured->alt_text }}" class="news-media__image">@else<img src="{{ asset('branding/logo-mark.svg') }}" alt="" aria-hidden="true">@endif
+    @if ($featured)<img src="{{ $featured->url() }}" alt="{{ $featured->alt_text }}" class="news-media__image">@else<img src="{{ asset('branding/logo-mark.png') }}" alt="" aria-hidden="true">@endif
 </div>
